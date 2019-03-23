e1004a69f74b2cf20770e8f1d69c2b44886874f8
Tommy Barter <tommybarter@hotmail.co.uk>
2019-03-23 13:13:45 +0000

    Setup deck and piles

--- uno/uno.py
+++ uno/uno.py
@@ -3,6 +3,7 @@ from tkinter import simpledialog
 from tkinter import ttk
 
 import os, os.path
+import random
 
 class GameWindow:
 
@@ -17,21 +18,21 @@ class GameWindow:
         w = self.background_image.width() 
         h = self.background_image.height() + 50
         self.master.geometry("%dx%d+0+0" % (w, h))
-        self.master.title("UNO GAME")                      
+        self.master.title("UNO GAME")        
+        self.master.resizable(False, False)
 
         self.canvas = Canvas(self.master)        
         self.canvas.pack(side='top', fill='both', expand='yes')
         self.canvas.create_image(0, 0, image=self.background_image, anchor=NW) 
 
 
-        self.startButton = Button(self.canvas, text="New Game", font=('Helvetica', 24), command=self.callbackNewGame, background="dark orange", foreground="white", height=5, width=20)
+        self.startButton = Button(self.canvas, text="New Game", font=('Helvetica', 24), command=self.callbackNewGame, background="dark orange", foreground="white")
         self.startButton.place(relx=0.5, rely=0.5, anchor=CENTER)
 
         self.main_menu(self.master)
-        status = StatusBar(self.master)
-        status.pack(side=BOTTOM, fill=X)
-        status.set("READY")
-        
+        self.status = StatusBar(self.master)
+        self.status.pack(side=BOTTOM, fill=X)
+        self.status.set("READY")        
        
        
     @staticmethod
@@ -69,10 +70,21 @@ class GameWindow:
             player2 = Player(optsDialog.player2)
             players.append(player2)     
         
-        self.discardPile = DiscardPile()
-        self.drawPile = DrawPile()
+        self.discardPile = DiscardPile() 
+        self.drawPile = DrawPile(FullDeck().getCards())
 
-        self.game = Game(players, self.discardPile, self.drawPile)
+        # Clear Board
+        self.startButton.destroy()
+
+        self.game = Game(players, self.discardPile, self.drawPile, self.canvas)
+
+        self.status.set("CURRENT PLAYER: " + self.game.currentPlayer.name)
+             
+
+        
+
+
+
 
     def callbackHelpAbout(self):
         print ("help about")
@@ -138,6 +150,8 @@ class DialogNewGame:
         b = Button(top, text="OK", command=self.ok)
         b.pack(pady=5)
 
+        self.ePlayer1.focus()
+
 
     def ok(self):
 
@@ -154,13 +168,16 @@ class Player:
         self.hand = None
         self.turn = False
         
-    def setName(playerName):
+    def setName(self, playerName):
         self.name = playerName
 
-    def setHand(thishand):
+    def setHand(self, thishand):
         self.hand = thishand
 
-    def setTurn(thisturn):
+    def getHand(self):
+        return self.hand
+
+    def setTurn(self, thisturn):
         self.turn = thisturn
 
     def placeCard():
@@ -171,63 +188,106 @@ class Player:
 
 
 class Card:
-    VALUES = ["0","1","2","3","4","5","6","7","8","9","Skip","Reverse","Draw2","Wild","Draw4"]    
-    COLOURS = ["Red","Yellow","Green","Blue","Wild"]
+    VALUES = ["0","1","2","3","4","5","6","7","8","9","Skip","Reverse","Draw2","Wild","Draw4"]
+    SKIP = 10
+    REVERSE = 11
+    DRAW2 = 12
+    WILD = 13
+    DRAW4 = 14
 
-    def __init__(self,val,col,sym,img,act,fac=False):
-        self.value = val
-        self.colour = col
-        self.image = img
-        self.action = act
+    COLOURS = ["Red","Yellow","Green","Blue","Wild"]
+    RED = 0
+    YELLOW = 1
+    GREEN = 2
+    BLUE = 3
+    ALL = 4
+
+    def __init__(self,val,col,fac=False):
+        self.value = self.VALUES[val]
+        self.colour = self.COLOURS[col]
         self.faceup = fac
 
-
     def get_name(self):
-        return Card.VALUE[self.value-1] + " of " + self.colour
+        return self.value + " of " + self.colour
 
     def get_image(self):
         pass
 
+    @staticmethod
+    def get_reverse_image():           
+        return os.path.join(os.getcwd(), 'Cards', 'Card_Back.png')
+
+    @staticmethod
+    def get_empty_image():           
+        return os.path.join(os.getcwd(), 'Cards', 'Card_Empty.png')
 
 # Deck is the super class for Hand, DrawPile and DiscardPile because they use the same array of cards.
 class Deck():
     MAXCARDS = 108
     
-    def __init__(self, thesecards):
+    def __init__(self, thesecards=[]):
         self.cards = thesecards
     
-    def removeCards(cards):
+    def removeCards(self, cards):
         pass
 
-    def addCards(cards):
+    def addCards(self, cards):
         pass
 
+    def takeCards(self, no_of_cards):
+        somecards = self.cards[:no_of_cards]
+        del self.cards[:no_of_cards]
+        return somecards
+    
     def turnAllCardsUp():
         pass
 
     def turnAllCardsDown():
         pass
 
+    def getCards(self):
+        return self.cards
+
+    def shuffleCards(self):
+        random.shuffle(self.cards)
+
+    def getNumberofCards(self):
+        return len(self.cards)
+
 class FullDeck(Deck):
     
     def __init__(self):
+        super()
+        self.cards = []
         # Add all cards
-       pass
+        colours = [0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,Card.DRAW2,Card.DRAW2,Card.REVERSE,Card.REVERSE,Card.SKIP,Card.SKIP]
+        wilds = [Card.WILD,Card.WILD,Card.WILD,Card.WILD,Card.DRAW4,Card.DRAW4,Card.DRAW4,Card.DRAW4]
+       
+        for card in colours:
+            self.cards.append(Card(card, Card.RED))
+            self.cards.append(Card(card, Card.GREEN))
+            self.cards.append(Card(card, Card.BLUE))
+            self.cards.append(Card(card, Card.YELLOW))
+
+        for wild in wilds:
+            self.cards.append(Card(wild, Card.ALL))        
 
 class Hand(Deck):
-   def __init__(self):
+   def __init__(self,thesecards=None):
+       self.cards = thesecards
        super()
-     #if playerTurn = True:
-            #fac = True
 
 class DrawPile(Deck):
-    def __init__(self):
+    def __init__(self,thesecards=None):
+       self.cards = thesecards
        super()
        
 
 class DiscardPile(Deck):
-    def __init__(self):
-       super()
+    def __init__(self,thesecards=None):
+        self.cards = thesecards
+        super()
+       
 
        #self.fac=True
 
@@ -235,16 +295,35 @@ class DiscardPile(Deck):
         pass
 
 
-class Game:
-    playerTurn = None
-
-    def __init__(self, theseplayers,thisDiscardPile,thisDrawPile):
+class Game:    
+    def __init__(self, theseplayers,thisDiscardPile,thisDrawPile,theCanvas):
         self.players = theseplayers
         self.discardPile = thisDiscardPile
         self.drawPile = thisDrawPile
+        self.canvas = theCanvas
 
+        # Shuffle the cards
+        self.drawPile.shuffleCards()
+
+        # Give players some cards
+        for player in self.players:
+            player.setHand(Hand(self.drawPile.takeCards(7)))
+            print(player.name)
+            for card in player.hand.getCards():
+                print(card.get_name())
+            
         # Select first player
-        self.chooseFirstPlayer()
+        self.currentPlayer = self.chooseFirstPlayer()
+
+        # Setup Board
+        self.reverse_image=PhotoImage(file=Card.get_reverse_image())
+        self.drawPileHolder = Label(self.canvas, text="Draw Pile", image=self.reverse_image,compound=BOTTOM)
+        self.drawPileHolder.place(relx=0.9, rely=0.5, anchor=CENTER)
+
+        self.empty_image=PhotoImage(file=Card.get_empty_image())
+        self.drawPileHolder = Label(self.canvas, text="Discard Pile", image=self.empty_image,compound=BOTTOM)
+        self.drawPileHolder.place(relx=0.5, rely=0.5, anchor=CENTER)
+
         # First move
         self.next_turn()
 
@@ -256,7 +335,7 @@ class Game:
         pass
 
     def chooseFirstPlayer(self):
-        pass
+        return random.choice(self.players)
 
     def endGame(self):
         pass
